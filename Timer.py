e5a0b0ec7397ccabff75b7e036088eb3ddcc2c31
ps <599103949@qq.com>
2018-04-09 03:46:09 +0800

    add button to change color of font and background

--- Timer.py
+++ Timer.py
@@ -1,19 +1,48 @@
 import sys
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QColorDialog, QLineEdit
 from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QFont
 from fromTodaytoTheTest import myTodaytoTheTest
-if __name__ == "__main__":      
+
+def changebackground():
+    color = QColorDialog.getColor()
+    if(color.isValid()):
+        w.setStyleSheet('QWidget{background-color:%s}' % color.name())
+#    else:
+#        print('quit')
+def changefontcolor():
+    color = QColorDialog.getColor()
+    if(color.isValid()):
+        a.setStyleSheet('color:%s' % color.name())
+#    else:
+#        pass
+        
+if __name__ == "__main__":
     app = QApplication(sys.argv)   
     w = QtWidgets.QWidget()
-    w.setGeometry(800,500,200,30)
-    w.setWindowOpacity(0.2)
+    w.setGeometry(700,500,460,80)
+    w.setWindowOpacity(0.5)
 #   w.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
-    w.setWindowFlags(QtCore.Qt.CustomizeWindowHint)
+    w.setWindowFlags(QtCore.Qt.CustomizeWindowHint|QtCore.Qt.NoDropShadowWindowHint)
+
     a = QtWidgets.QLabel(w)
-    a.setFont(QFont("Roman times", 20, QFont.Bold))
-    a.setText(' '+ myTodaytoTheTest()+ ' days left')
-    w.show() 
-    QTimer.singleShot(5*1000, app.quit)
+    a.setFont(QFont("Roman times", 50, QFont.Bold))
+    a.setText(' '+ myTodaytoTheTest()+ ' days left')    
+
+    colorbutton = QPushButton('b', w)
+    colorbutton.setGeometry(440, 0, 20, 80)
+    colorbutton.show()
+
+    fontbutton = QPushButton('f', w)
+    fontbutton.setGeometry(420, 0, 20, 80)
+    fontbutton.show()
+    
+    w.show()
+    colorbutton.clicked.connect(changebackground)
+
+    fontbutton.clicked.connect(changefontcolor)
+    
+
+#    QTimer.singleShot(5*1000, app.quit)
     app.exec_()
