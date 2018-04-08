6d6a252490169a7c0e77a519a7488fb8626668f8
ps <599103949@qq.com>
2018-04-09 01:03:44 +0800

    add opacity and font

--- Timer.py
+++ Timer.py
@@ -2,14 +2,17 @@ import sys
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QLabel
 from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QFont
 from fromTodaytoTheTest import myTodaytoTheTest
 if __name__ == "__main__":      
     app = QApplication(sys.argv)   
     w = QtWidgets.QWidget()
-    w.setGeometry(800,500,90,14)
+    w.setGeometry(800,500,200,30)
+    w.setWindowOpacity(0.2)
 #   w.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
     w.setWindowFlags(QtCore.Qt.CustomizeWindowHint)
     a = QtWidgets.QLabel(w)
+    a.setFont(QFont("Roman times", 20, QFont.Bold))
     a.setText(' '+ myTodaytoTheTest()+ ' days left')
     w.show() 
     QTimer.singleShot(5*1000, app.quit)
